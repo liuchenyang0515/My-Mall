20691b3af3eb624a90c12083acfe9873d8e38fc1
liuchenyang0515 <897418643@qq.com>
2021-03-06 22:01:51 +0800

    9-2 上线前准备工作(带前端)--相比上一节，这里补充了前端的东西

--- src/main/java/com/me/mall/config/MyMallWebMvcConfig.java
+++ src/main/java/com/me/mall/config/MyMallWebMvcConfig.java
@@ -13,7 +13,8 @@ public class MyMallWebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/admin/**").addResourceLocations("classpath:/static/admin/");
+        // 如果是admin开头的url就会被路由到/static/admin/目录下
+        registry.addResourceHandler("/admin/**").addResourceLocations("classpath:/static/admin/");
         // 只要是包含/images/** 的路径，都转发到${file.upload.dir}路径, 这里根据配置是D:/my-mall-prepare-static/, 方便上传图片后回显
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:" + Constant.FILE_UPLOAD_DIR);
